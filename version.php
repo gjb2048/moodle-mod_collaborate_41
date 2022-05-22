47aa88a49294b261c66943b89b56516806d7cdad
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-05-22 21:47:31 +0100

    Week 5 task 3.

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2022120408;
+$plugin->version = 2022120409;
 $plugin->requires  = 2022112800.00; // 4.1 (Build: 20221128).
 $plugin->supported = array(401, 401);
 $plugin->release = '401.1.5';
