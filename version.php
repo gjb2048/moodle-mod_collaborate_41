145084114f14fec36d726ca57aaf52334caf695a
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2022-05-30 17:29:32 +0100

    Week 6 task 2.

--- version.php
+++ version.php
@@ -30,7 +30,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_collaborate';
-$plugin->version = 2022120410;
+$plugin->version = 2022120411;
 $plugin->requires  = 2022112800.00; // 4.1 (Build: 20221128).
 $plugin->supported = array(401, 401);
 $plugin->release = '401.1.5';
